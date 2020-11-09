072b522b2dd1fe19b6077fd610d9142771c058c9
viniak-tft <gupta.viniak@tftus.com>
2020-11-09 15:15:34 +0530

    added cors rules

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// add cross domain
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
 //Import routes
 let apiRoutes = require("./routes")
 //Use API routes in the App
